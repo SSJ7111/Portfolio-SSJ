49d514892a44a701cba47467787eec2dbe943dde
Sukanya Jadhav <ssj0114.22@gmail.com>
2024-10-23 21:58:38 +0530

    new update

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -5,7 +5,7 @@ import { Socials } from "@/constants";
 const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full sm:h-[50px] flex flex-row items-center justify-between m-auto px-[10px]">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center"
@@ -24,7 +24,7 @@ const Navbar = () => {
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20 ">
-          <div className="flex flex-wrap items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          <div className="flex flex-wrap xs:h-[40px] items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
             </a>
@@ -37,7 +37,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex flex-row gap-5 sm:gap-2">
+        <div className="flex flex-row gap-5 xs:gap-2">
           {Socials.map((social) => (
             <Image
               src={social.src}
